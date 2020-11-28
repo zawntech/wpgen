8bb20e62b82ce2f6ddee6eac674c36a5fcfef08a
Marty Eason <martye@sideways8.com>
2020-11-28 13:20:55 -0500

    Update create:api description text

--- config/api-options.php
+++ config/api-options.php
@@ -3,7 +3,7 @@ return [
     [
         'key' => 'api_namespace',
         'label' => 'Api Namespace',
-        'description' => 'A unique namespace for your API; ie: "my-project" (wp-json/{namespace}/v1/)',
+        'description' => 'A unique namespace for your API; ie: my-project resolves to /wp-json/my-project/v1/',
         'type' => 'string'
     ],
 ];
