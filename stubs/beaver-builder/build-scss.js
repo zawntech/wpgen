f469e74f2347147ff2384f42ed9bc18f6aab1938
Alleged Wizard <marty@allegedwizard.com>
2024-09-26 15:38:07 -0400

    Update build-scss.js

--- stubs/beaver-builder/build-scss.js
+++ stubs/beaver-builder/build-scss.js
@@ -1,29 +1,33 @@
 const fs = require('fs');
-const path = require('path');
-const buildScss = require('node-sass');
-const glob = require('glob-promise');
-const bbModulesPath = path.resolve(__dirname + '/../../bb-modules/');
-const target = `${bbModulesPath}/**/*.scss`;
+const buildScss = require('sass');
+const glob = require('glob');
+const target = `bb-modules/**/*.scss`;
 const md5 = require('md5');
 
 glob(target).then(processFiles);
 
 async function processFiles(files) {
-    for (let i in files) {
-        const file = files[i],
-            outFile = file.replace('.scss', '.css');
-        let hash;
-        if (fs.existsSync(outFile)) {
-            hash = md5(fs.readFileSync(outFile, 'utf8'));
-        }
-        await buildScss.render({file, outFile}, function (err, result) {
-            let newHash = md5(result.css);
-            if (hash !== newHash) {
-                fs.writeFile(outFile, result.css, function (err) {
-                    if (!err) {
-                    }
-                });
-            }
-        });
+
+  for (let i in files) {
+
+    const file = files[i];
+    const outFile = file.replace('.scss', '.css');
+
+    // Create a hash of the existing output to see if recompilation is necessary;
+    let hash;
+    if (fs.existsSync(outFile)) {
+      hash = md5(fs.readFileSync(outFile, 'utf8'));
     }
+
+    // Compile sass.
+    try {
+      const result = buildScss.compile(file);
+      const newHash = md5(result.css);
+      if (hash !== newHash) {
+        fs.writeFile(outFile, result.css);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
