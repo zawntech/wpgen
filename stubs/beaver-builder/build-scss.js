5318d626b562aa5dfcd63a4f1e2e562a1949490b
Alleged Wizard <marty@allegedwizard.com>
2024-11-16 15:41:42 -0500

    Update build-scss.js

--- stubs/beaver-builder/build-scss.js
+++ stubs/beaver-builder/build-scss.js
@@ -8,26 +8,26 @@ glob(target).then(processFiles);
 
 async function processFiles(files) {
 
-  for (let i in files) {
+    for (let i in files) {
 
-    const file = files[i];
-    const outFile = file.replace('.scss', '.css');
+        const file = files[i];
+        const outFile = file.replace('.scss', '.css');
 
-    // Create a hash of the existing output to see if recompilation is necessary;
-    let hash;
-    if (fs.existsSync(outFile)) {
-      hash = md5(fs.readFileSync(outFile, 'utf8'));
+        // Create a hash of the existing output to see if recompilation is necessary;
+        let hash;
+        if (fs.existsSync(outFile)) {
+            hash = md5(fs.readFileSync(outFile, 'utf8'));
+        }
+
+        // Compile sass.
+        try {
+            const result = buildScss.compile(file);
+            const newHash = md5(result.css);
+            if (!hash || hash !== newHash) {
+                fs.writeFile(outFile, result.css, {}, () => {});
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
-
-    // Compile sass.
-    try {
-      const result = buildScss.compile(file);
-      const newHash = md5(result.css);
-      if (hash !== newHash) {
-        fs.writeFile(outFile, result.css);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
